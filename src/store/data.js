d25a42b7f20d55dd81d99f224df86644974176bc
krystek77 <krystian.wrona77@gmail.com>
2020-06-18 08:26:33 +0200

    prepare dummy data

---
+++ src/store/data.js
@@ -0,0 +1,102 @@
+export const porducts = [
+  {
+    id: 1,
+    img: "img/banana-min.png",
+    price: 2,
+    count: 0,
+    total: 0,
+    description:
+      "Objectively harness interactive materials without standards compliant materials. Conveniently deploy business intellectual capital rather than cutting-edge materials. Globally develop parallel vortals through just.",
+    name: "Banana fresh",
+    origin: "Poland",
+  },
+  {
+    id: 2,
+    img: "img/cucumber-min.png",
+    price: 1,
+    count: 0,
+    total: 0,
+    description:
+      "Objectively harness interactive materials without standards compliant materials. Conveniently deploy business intellectual capital rather than cutting-edge materials. Globally develop parallel vortals through just.",
+    name: "Banana fresh",
+    origin: "Poland",
+  },
+  {
+    id: 3,
+    img: "img/lemon-min.png",
+    price: 3.2,
+    count: 0,
+    total: 0,
+    description:
+      "Objectively harness interactive materials without standards compliant materials. Conveniently deploy business intellectual capital rather than cutting-edge materials. Globally develop parallel vortals through just.",
+    name: "Lemon",
+    origin: "Spain",
+  },
+  {
+    id: 4,
+    img: "img/onion-min.png",
+    price: 1.2,
+    count: 0,
+    total: 0,
+    description:
+      "Objectively harness interactive materials without standards compliant materials. Conveniently deploy business intellectual capital rather than cutting-edge materials. Globally develop parallel vortals through just.",
+    name: "Young onion",
+    origin: "Germany",
+  },
+  {
+    id: 5,
+    img: "img/orange-min.png",
+    price: 1.5,
+    count: 0,
+    total: 0,
+    description:
+      "Objectively harness interactive materials without standards compliant materials. Conveniently deploy business intellectual capital rather than cutting-edge materials. Globally develop parallel vortals through just.",
+    name: "juicy orange",
+    origin: "Brasil",
+  },
+  {
+    id: 6,
+    img: "img/pepper-min.png",
+    price: 4.2,
+    count: 0,
+    total: 0,
+    description:
+      "Objectively harness interactive materials without standards compliant materials. Conveniently deploy business intellectual capital rather than cutting-edge materials. Globally develop parallel vortals through just.",
+    name: "Red pepper",
+    origin: "Poland",
+  },
+  {
+    id: 7,
+    img: "img/potato-min.png",
+    price: 2,
+    count: 0,
+    total: 0,
+    description:
+      "Objectively harness interactive materials without standards compliant materials. Conveniently deploy business intellectual capital rather than cutting-edge materials. Globally develop parallel vortals through just.",
+    name: "Awesome potato",
+    origin: "Columbia",
+  },
+  {
+    id: 8,
+    img: "img/watermelon-min.png",
+    price: 2,
+    count: 0,
+    total: 0,
+    description:
+      "Objectively harness interactive materials without standards compliant materials. Conveniently deploy business intellectual capital rather than cutting-edge materials. Globally develop parallel vortals through just.",
+    name: "Water melon",
+    origin: "USA",
+  },
+];
+
+export const detailProduct = {
+  id: 6,
+  img: "img/pepper-min.png",
+  price: 4.2,
+  count: 0,
+  total: 0,
+  description:
+    "Objectively harness interactive materials without standards compliant materials. Conveniently deploy business intellectual capital rather than cutting-edge materials. Globally develop parallel vortals through just.",
+  name: "Red pepper",
+  origin: "Poland",
+};
