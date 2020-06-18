a6dbc618349ae3b910c7d374881d9dd10f7431ad
krystek77 <krystian.wrona77@gmail.com>
2020-06-18 08:14:38 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { ThemeContextConsumer } from "./context/theme";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    purchases: [],
+  };
+  componentDidMount() {
+    this.getPurchases();
+  }
+
+  getPurchases = async () => {
+    const response = await fetch(
+      "https://purchase-list-688c1.firebaseio.com/purchases.json"
+    );
+    if (response.status === 200) {
+      const responseData = await response.json();
+      // console.log(responseData);
+      const formattedResponseData = [];
+      for (let key in responseData) {
+        formattedResponseData.push({ id: key, ...responseData[key] });
+      }
+      // console.log(formattedResponseData);
+      this.setState(
+        () => {
+          return {
+            purchases: formattedResponseData,
+          };
+        },
+        () => {
+          console.log("Updated state now", this.state.purchases);
+        }
+      );
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <p>Hello Cart</p>
+        <ThemeContextConsumer>
+          {(val) => {
+            return (
+              <React.Fragment>
+                <p style={{ color: val.theme.primary.main }}>I am a cameleon</p>
+                <button
+                  type="button"
+                  onClick={() => val.toggleTheme()}
+                  style={{
+                    backgroundColor: val.theme.primary.main,
+                    color: val.theme.primary.text,
+                    border: `1px solid ${val.theme.primary.dark}`,
+                    borderRadius: "5px",
+                    padding: "10px 15px",
+                    cursor: "pointer",
+                  }}
+                >
+                  Switch theme button
+                </button>
+                {this.state.purchases.map((purchase) => {
+                  return (
+                    <p
+                      style={{ border: `1px solid ${val.theme.primary.dark}` }}
+                      key={purchase.id}
+                    >
+                      {purchase.name}
+                    </p>
+                  );
+                })}
+              </React.Fragment>
+            );
+          }}
+        </ThemeContextConsumer>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/context/theme.js
@@ -0,0 +1,85 @@
+import React from "react";
+
+const themes = {
+  orangeMagenta: {
+    primary: {
+      main: "#8B008B",
+      light: "#BE46BC",
+      dark: "#59005D",
+      text: "#FFFFFF",
+    },
+    secondary: {
+      main: "#FFA600",
+      light: "#FFD74A",
+      dark: "#C67700",
+      text: "#000000",
+    },
+  },
+  magentaOrange: {
+    primary: {
+      main: "#FFA600",
+      light: "#FFD74A",
+      dark: "#C67700",
+      text: "#000000",
+    },
+    secondary: {
+      main: "#8B008B",
+      light: "#BE46BC",
+      dark: "#59005D",
+      text: "#FFFFFF",
+    },
+  },
+};
+
+const ThemeContext = React.createContext({
+  theme: themes.orangeMagenta,
+  toggleTheme: () => {},
+});
+
+class ThemeContextProvider extends React.Component {
+  constructor(props) {
+    console.log("[ThemeContextProvider] - constructor");
+    super(props);
+    this.state = {
+      theme: themes.orangeMagenta,
+    };
+  }
+  componentDidUpdate() {
+    console.log("[ThemeContextProvider] - updated");
+  }
+  componentDidMount() {
+    console.log("[ThemeContextProvider] - mounted");
+  }
+
+  toggleTheme() {
+    console.log("TOGGLE THEME");
+    this.setState(
+      (prevState, props) => {
+        console.log("[ThemeContextProvider] - setState");
+        return {
+          theme:
+            prevState.theme === themes.orangeMagenta
+              ? themes.magentaOrange
+              : themes.orangeMagenta,
+        };
+      },
+      () => {
+        console.log("Theme changed", this.state.theme);
+      }
+    );
+  }
+  render() {
+    console.log("[ThemeContextProvider] - render", this.state.theme);
+    const { children } = this.props;
+    return (
+      <ThemeContext.Provider
+        value={{ ...this.state, toggleTheme: this.toggleTheme.bind(this) }}
+      >
+        {children}
+      </ThemeContext.Provider>
+    );
+  }
+}
+const ThemeContextConsumer = ThemeContext.Consumer;
+
+export { ThemeContextProvider, ThemeContextConsumer };
