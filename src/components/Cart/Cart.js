35cb36f004056f39c0800ff3f786dbc1998ec6ca
krystek77 <krystian.wrona77@gmail.com>
2020-06-19 14:35:14 +0200

    start create cart page

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,35 @@
 import React from "react";
+import Title from "../Title";
+import { ThemeContextConsumer } from "../../context/theme";
+import { ProductContextConsumer } from "../../context/product";
 
 export default function Cart() {
-  return <div>Cart Page</div>;
+  return (
+    <ThemeContextConsumer>
+      {(theme) => {
+        return (
+          <ProductContextConsumer>
+            {(data) => {
+              let title = "Buy something please. I am so much empty and sad!";
+              let content = null;
+              if (data.cart.length > 0) {
+                title = "Your cart is not empty. I'm so happy now ...";
+                content = data.cart.map((item) => {
+                  return <p key={item.id}>{item.name}</p>;
+                });
+              }
+              return (
+                <div className="container">
+                  <div className="row">
+                    <Title title={title} />
+                  </div>
+                  {content}
+                </div>
+              );
+            }}
+          </ProductContextConsumer>
+        );
+      }}
+    </ThemeContextConsumer>
+  );
 }
