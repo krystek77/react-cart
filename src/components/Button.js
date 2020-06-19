b517f97f37913d16befcbb630cc0af0d8a074557
krystek77 <krystian.wrona77@gmail.com>
2020-06-19 22:19:20 +0200

    add other styles for button on card page

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 export const ButtonWrapper = styled.button`
   font-size: ${(props) => (props.navbar ? "1.6rem" : "1.1rem")};
-  padding: ${(props) => (props.navbar ? "0" : "0.5rem 0.8rem")};
+  padding: ${(props) =>
+    props.navbar ? "0" : props.cart ? "0.3rem 0.5rem" : "0.3rem 0.8rem"};
   margin: 0.2rem;
   border-radius: 0.2rem;
   cursor: pointer;
@@ -15,6 +16,8 @@ export const ButtonWrapper = styled.button`
       ? props.value.theme.primary.main
       : props.navbar
       ? "transparent"
+      : props.cart
+      ? props.value.theme.primary.main
       : props.value.theme.secondary.main};
   border: 1px solid
     ${(props) =>
@@ -22,6 +25,8 @@ export const ButtonWrapper = styled.button`
         ? props.value.theme.primary.dark
         : props.navbar
         ? "transparent"
+        : props.cart
+        ? props.value.theme.primary.main
         : props.value.theme.secondary.dark};
   transition: all 0.3s ease-in-out;
   &:disabled {
@@ -34,6 +39,8 @@ export const ButtonWrapper = styled.button`
   &:hover {
     color: ${(props) =>
       props.modal
+        ? props.value.theme.primary.dark
+        : props.cart
         ? props.value.theme.primary.dark
         : props.value.theme.secondary.dark};
   }
