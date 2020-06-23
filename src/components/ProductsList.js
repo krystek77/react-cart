dd618a6c570b011cfbe2b8a06bb0e73d65084eb6
krystek77 <krystian.wrona77@gmail.com>
2020-06-23 09:34:07 +0200

    display error for bad get products request

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -2,6 +2,7 @@ import React from "react";
 import Product from "./Product";
 import Title from "./Title";
 import { ProductContextConsumer } from "../context/product";
+import { Error } from "./ErrorWrapper";
 
 export default function ProductsList() {
   return (
@@ -12,9 +13,13 @@ export default function ProductsList() {
       <div className="row">
         <ProductContextConsumer>
           {(value) => {
-            return value.products.map((product) => {
+            console.log(value);
+            let productsList = value.products.map((product) => {
               return <Product key={product.id} {...product} />;
             });
+            if (value.error.message)
+              productsList = <Error error={value.error} />;
+            return productsList;
           }}
         </ProductContextConsumer>
       </div>
