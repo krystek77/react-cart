e40e935a5491ab88130780d4ac62e87d4a9b779d
krystek77 <krystian.wrona77@gmail.com>
2020-06-24 07:36:33 +0200

    Update cart.js
    Adding context to lifecycle method to fetch cart items for specyfic user.
    Adding spinner and error components. Refactor code structure.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,64 +3,76 @@ import Title from "../Title";
 import CartItem from "./CartItem";
 import CartHeader from "./CartHeader";
 import Summary from "./Summary";
-import { ProductContextConsumer } from "../../context/product";
+import { ProductContext } from "../../context/product";
 import { ThemeContextConsumer } from "../../context/theme";
 import { ButtonWrapper } from "../Button";
+import Spinner from "../CssSpinner";
+import Error from "../ErrorWrapper";
 
-export default function Cart() {
-  return (
-    <ProductContextConsumer>
-      {(data) => {
-        let title = "Buy something please. I am so much empty and sad!";
-        let content = null;
-        if (data.cart.length > 0) {
-          title = "Your cart is not empty. I'm so happy now ...";
-          content = (
-            <React.Fragment>
-              <CartHeader />
-              {data.cart.map((item) => {
-                return (
-                  <CartItem
-                    key={item.id}
-                    {...item}
-                    remove={data.removeProduct}
-                    increase={data.increaseProduct}
-                    decrease={data.decreaseProduct}
-                  />
-                );
-              })}
-              <ThemeContextConsumer>
-                {(theme) => (
-                  <React.Fragment>
-                    <div className="row">
-                      <ButtonWrapper
-                        clear
-                        value={theme}
-                        onClick={() => data.clearCart()}
-                      >
-                        Clear cart
-                      </ButtonWrapper>
-                    </div>
-                    <div className="row">
-                      <Summary value={theme} total={data.total} />
-                    </div>
-                  </React.Fragment>
-                )}
-              </ThemeContextConsumer>
-            </React.Fragment>
-          );
-        }
-        return (
-          <React.Fragment>
-            <div className="container">
-              <div className="row">
-                <Title title={title} />
-              </div>
+export default class Cart extends React.Component {
+  componentDidMount() {
+    console.log("[Cart] - mounted");
+    this.context.getCartItems();
+  }
+
+  render() {
+    let title = "Buy something please. I am so much empty and sad!";
+    let content = null;
+    if (this.context.error.message)
+      content = <Error error={this.context.error} />;
+    if (this.context.isLoading) content = <Spinner />;
+
+    if (this.context.cart.length > 0) {
+      title = "Your cart is not empty. I'm so happy now ...";
+      content = (
+        <React.Fragment>
+          <CartHeader />
+          {this.context.cart.map((item) => {
+            return (
+              <CartItem
+                key={item.id}
+                {...item}
+                remove={this.context.removeProduct}
+                increase={this.context.increaseProduct}
+                decrease={this.context.decreaseProduct}
+              />
+            );
+          })}
+          <ThemeContextConsumer>
+            {(theme) => (
+              <React.Fragment>
+                <div className="row">
+                  <ButtonWrapper
+                    clear
+                    value={theme}
+                    onClick={() => this.context.clearCart()}
+                  >
+                    Clear cart
+                  </ButtonWrapper>
+                </div>
+                <div className="row">
+                  <Summary value={theme} total={this.context.total} />
+                </div>
+              </React.Fragment>
+            )}
+          </ThemeContextConsumer>
+        </React.Fragment>
+      );
+    }
+
+    return (
+      <React.Fragment>
+        {!this.context.isLoading || !this.context.error.message ? null : (
+          <div className="container">
+            <div className="row">
+              <Title title={title} />
             </div>
-            <div className="container">{content}</div>
-          </React.Fragment>
-        );
-      }}
-    </ProductContextConsumer>
-  );
+          </div>
+        )}
+        <div className="container">{content}</div>
+      </React.Fragment>
+    );
+  }
 }
+
+Cart.contextType = ProductContext;
