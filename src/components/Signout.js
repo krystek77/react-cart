0d44704573b9c975b60b403b7debfad350650648
krystek77 <krystian.wrona77@gmail.com>
2020-06-24 07:41:24 +0200

    Update signout.js
    Setting emtpty cart if user logout.

--- src/components/Signout.js
+++ src/components/Signout.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
+import { ProductContext } from "../context/product";
 
 class Signout extends Component {
   componentDidMount() {
     this.props.signout();
+    this.context.clearCart();
   }
   render() {
     return <Redirect to="/" />;
   }
 }
+
+Signout.contextType = ProductContext;
+
 export default Signout;
