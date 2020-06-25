e2bcd6e239377b47b631eabc1ef0f73f6f5cc577
krystek77 <krystian.wrona77@gmail.com>
2020-06-25 13:35:21 +0200

    refactor productLists component to use more context

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -1,27 +1,44 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import Product from "./Product";
 import Title from "./Title";
-import { ProductContextConsumer } from "../context/product";
+import { ProductContext } from "../context/product";
+import { AuthContext } from "../context/auth";
 import Error from "./ErrorWrapper";
 import CssSpinner from "./CssSpinner";
 
 export default function ProductsList() {
+  const productContext = useContext(ProductContext);
+  const authContext = useContext(AuthContext);
+
+  let productsList = productContext.products.map((product) => {
+    return <Product key={product.id} {...product} />;
+  });
+  if (productContext.error.message)
+    productsList = <Error error={productContext.error} />;
+  if (productContext.isLoading) productsList = <CssSpinner />;
+
+  useEffect(() => {
+    console.log("[ProductsList.js]-mounted");
+    if (authContext.idToken !== "") {
+      productContext.getCartItems();
+    } else {
+      productContext.getProducts();
+    }
+    return () => {
+      console.log("[ProductsList.js]-unmounted");
+    };
+  }, [
+    authContext.idToken,
+    productContext.getCartItems,
+    productContext.getProducts,
+  ]);
+
   return (
     <div className="container">
       <div className="row">
         <Title title="our fresh, healthy, tasty fruits and vegetables" />
       </div>
-
-      <ProductContextConsumer>
-        {(value) => {
-          let productsList = value.products.map((product) => {
-            return <Product key={product.id} {...product} />;
-          });
-          if (value.error.message) productsList = <Error error={value.error} />;
-          if (value.isLoading) productsList = <CssSpinner />;
-          return <div className="row">{productsList}</div>;
-        }}
-      </ProductContextConsumer>
+      <div className="row">{productsList}</div>;
     </div>
   );
 }
